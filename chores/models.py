350b1141ff592ef9e2464dff2ee7ad52d92696b2
Paul Lubasi <akufunal.zm@gmail.com>
2026-09-09 14:43:14 +0200

    feat: add household member and chore models

--- chores/models.py
+++ chores/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class HouseholdMember(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Chore(models.Model):
+    title = models.CharField(max_length=200)
+    assignee = models.ForeignKey(
+        HouseholdMember,
+        on_delete=models.PROTECT,
+        related_name="chores",
+    )
+    due_date = models.DateField()
+    is_complete = models.BooleanField(default=False)
